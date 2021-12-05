309f48077922b5b7e9c387dbee805f5aa64ad98a
Alex <kelganor@gmail.com>
2021-12-05 21:17:22 +0100

    Remove redundant code
    Update day 5 code - use single functions for parts 1 and 2

--- solver_05.py
+++ solver_05.py
@@ -1,22 +1,7 @@
 # aoc 2021-05
 
 
-def solve(lines):
-    d = {}
-    for x1, y1, x2, y2 in lines:
-        if x1 == x2 and y1 == y2:
-            d[(x1, y1)] = d.get((x1, y1), 0) + 1
-        elif x1 == x2:
-            for y in range(min(y1, y2), max(y1, y2) + 1):
-                d[(x1, y)] = d.get((x1, y), 0) + 1
-        elif y1 == y2:
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                d[(x, y1)] = d.get((x, y1), 0) + 1
-    res = sum([1 for x in d.values() if x > 1])
-    return res
-
-
-def solve2(lines):
+def solve(lines, skip_diags=False):
     d = {}
     for x1, y1, x2, y2 in lines:
         dx, dy = x2 - x1, y2 - y1
@@ -24,6 +9,8 @@ def solve2(lines):
         if dy != 0: dy = dy/abs(dy)
         x, y = x1, y1
         while x != x2 + dx or y != y2 + dy:
+            if skip_diags and (x1 != x2 and y1 != y2):
+                break
             d[(x, y)] = d.get((x, y), 0) + 1
             x += dx
             y += dy
@@ -43,5 +30,5 @@ if __name__ == "__main__":
         x2, y2 = map(int, p2.split(','))
         data.append((x1, y1, x2, y2))
 
-    print('p1: ', solve(data))
-    print('p2: ', solve2(data))
+    print('p1: ', solve(data, skip_diags=True))
+    print('p2: ', solve(data))
